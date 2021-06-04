7c5f4a05e2fedf9a62ee4fddddad93fd5641edd0
marufsiddique01 <marufsiddique71@yahoo.com>
2021-06-05 02:34:52 +0600

    done with headers yo

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
 import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
 
 function Header() {
   return (
     <div className="header">
-      <h2>I am the headers</h2>
       <IconButton>
         <PersonIcon fontSize="large" className="header_icon" />
+      </IconButton>
+
+      <img
+        className="header_logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png"
+        alt=""
+      />
+
+      <IconButton>
+        <ForumIcon fontSize="large" className="forum_icon" />
       </IconButton>
     </div>
   );
