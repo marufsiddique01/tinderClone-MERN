839e8e760e6e3d29a9a3928243891fde32d4f6e5
marufsiddique01 <marufsiddique71@yahoo.com>
2021-06-05 02:40:22 +0600

    tindercards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       {/* Header */}
       <Header />
       {/* TinderCards */}
+      <TinderCards />
+
       {/* lower swipe buttons */}
     </div>
   );
